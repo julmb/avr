27272a43fdcab146110c466e848c88c2335f9bf8
Julian Brunner <julian.brunner@gmail.com>
2013-08-07 01:14:14 +0200

    removed includes from main templates

--- main_atmega328P.c
+++ main_atmega328P.c
@@ -1,8 +1,5 @@
 #define F_CPU 16000000UL
 
-#include "port.h"
-#include "usart.h"
-
 int main()
 {
 	return 0;
--- main_attiny85.c
+++ main_attiny85.c
@@ -1,8 +1,5 @@
 #define F_CPU 16000000UL
 
-#include "port.h"
-#include "spi_usi.h"
-
 int main()
 {
 	return 0;
