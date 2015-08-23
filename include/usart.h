40b299386600b2b499c285a7c1d1443e91f59f52
Julian Brunner <julian.brunner@gmail.com>
2015-08-23 18:18:52 +0200

    added baud rate and double speed parameters to USART module

--- include/usart.h
+++ include/usart.h
@@ -27,11 +27,10 @@ void usart_rx_read_bytes(uint8_t* data, uint16_t data_length)
 	for (uint16_t index = 0; index < data_length; index++) usart_rx_read_byte(&data[index]);
 }
 
-// TODO: pass baud rate and maybe other parameters
-void usart_initialize()
+void usart_initialize(uint16_t baud_rate, uint8_t double_speed)
 {
 	// set UBBR0 to 2 for 833333 baud at 20 MHz CPU clock in double speed mode
-	UBRR0 = 0x0002;
+	UBRR0 = baud_rate;
 
 	// enable double speed mode (U2X0 = 0b1)
 	// disable multi-processor communication mode (MPCM0 = 0b0)
@@ -42,12 +41,15 @@ void usart_initialize()
 	// set stop bit mode to 1 stop bit (USBS0 = 0b0)
 	// set character size to 8 bit (UCSZ0 = 0b011)
 	// set clock polarity bit to zero, not used in asynchronous mode (UCPOL0 = 0b0)
-	UCSR0A = (1 << U2X0) | (0 << MPCM0);
+	UCSR0A = ((double_speed ? 1 : 0) << U2X0) | (0 << MPCM0);
 	UCSR0B = (0 << RXCIE0) | (0 << TXCIE0) | (0 << UDRIE0) | (1 << RXEN0) | (1 << TXEN0) | (0 << UCSZ02);
 	UCSR0C = (0 << UMSEL01) | (0 << UMSEL00) | (0 << UPM01) | (0 << UPM00) | (0 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00) | (0 << UCPOL0);
 }
 void usart_dispose()
 {
+	// reset baud rate
+	UBBR0 = 0x0000;
+
 	// reset USART configuration and status
 	UCSR0A = 0x00;
 	UCSR0B = 0x00;
