b715ebe40ab0b8411310e33298c3bd2b7c20dcf4
Julian Brunner <julian.brunner@gmail.com>
2015-10-12 23:57:23 +0200

    enabled parametrization of receiver/transmitter enabledness in usart module

--- include/usart.h
+++ include/usart.h
@@ -27,23 +27,21 @@ void usart_rx_read_bytes(uint8_t* data, uint16_t data_length)
 	for (uint16_t index = 0; index < data_length; index++) usart_rx_read_byte(&data[index]);
 }
 
-void usart_initialize(uint16_t baud_rate, uint8_t double_speed)
+void usart_initialize(uint8_t receiver, uint8_t transmitter, uint16_t baud_rate, uint8_t double_speed)
 {
-	// set UBBR0 to 2 for 833333 baud at 20 MHz CPU clock in double speed mode
-	UBRR0 = baud_rate;
-
-	// enable double speed mode (U2X0 = 0b1)
+	// set double speed mode (U2X0 = double_speed)
 	// disable multi-processor communication mode (MPCM0 = 0b0)
 	// disable all interrupts (RXCIE0 = 0b0, TXCIE0 = 0b0, UDRIE0 = 0b0)
-	// enable receiver and transmitter (RXEN0 = 0b1, TXEN0 = 0b1)
+	// set receiver and transmitter (RXEN0 = receiver, TXEN0 = transmitter)
 	// set mode to asynchronous USART (UMSEL0 = 0b00)
 	// set parity mode to disabled (UPM0 = 0b00)
 	// set stop bit mode to 1 stop bit (USBS0 = 0b0)
 	// set character size to 8 bit (UCSZ0 = 0b011)
 	// set clock polarity bit to zero, not used in asynchronous mode (UCPOL0 = 0b0)
 	UCSR0A = ((double_speed ? 1 : 0) << U2X0) | (0 << MPCM0);
-	UCSR0B = (0 << RXCIE0) | (0 << TXCIE0) | (0 << UDRIE0) | (1 << RXEN0) | (1 << TXEN0) | (0 << UCSZ02);
+	UCSR0B = (0 << RXCIE0) | (0 << TXCIE0) | (0 << UDRIE0) | ((receiver ? 1 : 0) << RXEN0) | ((transmitter ? 1 : 0) << TXEN0) | (0 << UCSZ02);
 	UCSR0C = (0 << UMSEL01) | (0 << UMSEL00) | (0 << UPM01) | (0 << UPM00) | (0 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00) | (0 << UCPOL0);
+	UBRR0 = baud_rate;
 }
 void usart_dispose()
 {
