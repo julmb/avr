67694f258bcb9f4bed74e0e7978d77e8b5b97470
Julian Brunner <julian.brunner@gmail.com>
2015-10-31 08:28:02 +0100

    added basic module

---
+++ modules/basic.h
@@ -0,0 +1,39 @@
+#ifndef basic_h
+#define basic_h
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <avr/wdt.h>
+
+typedef enum { unknown, power, external, software, brown_out } reset_type;
+
+reset_type initialize()
+{
+	uint8_t status = MCUSR;
+
+	MCUSR = 0x00;
+
+	wdt_disable();
+
+	if (status == _BV(PORF)) return power;
+	if (status == _BV(EXTRF)) return external;
+	if (status == _BV(WDRF)) return software;
+	if (status == _BV(BORF)) return brown_out;
+
+	return unknown;
+}
+
+void reset()
+{
+	wdt_enable(WDTO_15MS);
+
+	while (1);
+}
+
+void application()
+{
+	asm("jmp 0");
+}
+
+#endif
