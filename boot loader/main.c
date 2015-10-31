b78f59ce8045a448a0689dd7a5cabfa83bfc16be
Julian Brunner <julian.brunner@gmail.com>
2015-10-31 08:29:45 +0100

    continued working on boot loader

--- boot loader/main.c
+++ boot loader/main.c
@@ -1,22 +1,96 @@
-#include <stddef.h>
-#include <stdint.h>
-
+#include <basic.h>
 #include <usart.h>
-
 #include <display.h>
-void main()
+
+#include <avr/pgmspace.h>
+#include <avr/boot.h>
+
+// TODO: remove display
+// TODO: remove unused includes
+// TODO: enable reading/writing all the pieces of memory that avrdude supports as well
+// display_printf(display, "BL: 0x%04X", reset_type);
+
+FUSES =
+{
+.low = LFUSE_DEFAULT,
+.high = HFUSE_DEFAULT,
+.extended = EFUSE_DEFAULT
+};
+
+typedef enum { read = 0x01, write = 0x02 } command;
+typedef enum { flash = 0x01, eeprom = 0x02 } memory;
+
+uint8_t read_byte(void* position)
+{
+	return pgm_read_byte(position);
+}
+void write_page(void* position, void* data)
+{
+	uint8_t* bytes = data;
+
+	boot_page_erase(position);
+	boot_spm_busy_wait();
+
+	for (uint8_t byte_index = 0; byte_index < SPM_PAGESIZE; byte_index += 2)
+		boot_page_fill(position + byte_index, (*bytes++ << 0) | (*bytes++ << 8));
+
+	boot_page_write(position);
+	boot_spm_busy_wait();
+
+	boot_rww_enable();
+}
+
+void loader()
 {
 	display display = display_initialize(pin_initialize(port_initialize(port_c), 5), pin_initialize(port_initialize(port_c), 4), pin_initialize(port_initialize(port_c), 3), port_initialize(port_d));
+	display_printf(display, "loader %u", sizeof(memory));
 
-	display_printf(display, "lulz: %02X", 0x1F);
-
-	// enables USART with a divider of 64, giving about 1 kbaud/s/MHz
+	// enable USART with a divider of 64 * 16, giving about 1 kBd/MHz
 	usart_initialize(1, 1, 0x003F, 0);
 
 	while (1)
 	{
-		usart_wait();
+		command command;
+		if (usart_read(&command, sizeof(command))) continue;
+
+		memory memory;
+		if (usart_read(&memory, sizeof(memory))) continue;
+
+		void* position;
+		if (usart_read(&position, sizeof(position))) continue;
+
+		switch (command)
+		{
+			case read:
+				switch (memory)
+				{
+					case flash:
+					{
+						uint8_t byte = read_byte(position);
+						usart_write(&byte, sizeof(byte));
+						break;
+					}
+				}
+				break;
+			case write:
+				switch (memory)
+				{
+					case flash: break;
+				}
+				break;
+		}
 	}
 
 	usart_dispose();
+}
+
+void main()
+{
+	reset_type reset_type = initialize();
+
+	if (reset_type == external) loader();
+
+	display display = display_initialize(pin_initialize(port_initialize(port_c), 5), pin_initialize(port_initialize(port_c), 4), pin_initialize(port_initialize(port_c), 3), port_initialize(port_d));
+	display_printf(display, "APP");
+	application();
 }
