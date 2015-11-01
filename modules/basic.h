ec96613795ed0a2de48eaeddb089460afffc9176
Julian Brunner <julian.brunner@gmail.com>
2015-11-01 15:17:22 +0100

    added memory constants

--- modules/basic.h
+++ modules/basic.h
@@ -6,6 +6,19 @@
 
 #include <avr/wdt.h>
 
+#define FLASH_BASE ((size_t)0)
+#define FLASH_SIZE (((size_t)FLASHEND + 1) - FLASH_BASE)
+#define FLASH_PAGE_SIZE ((size_t)SPM_PAGESIZE)
+#define FLASH_PAGE_COUNT (FLASH_SIZE / FLASH_PAGE_SIZE)
+
+#define EEPROM_BASE ((size_t)0)
+#define EEPROM_SIZE (((size_t)E2END + 1) - EEPROM_BASE)
+#define EEPROM_PAGE_SIZE ((size_t)E2PAGESIZE)
+#define EEPROM_PAGE_COUNT (EEPROM_SIZE / EEPROM_PAGE_SIZE)
+
+#define SRAM_BASE ((size_t)RAMSTART)
+#define SRAM_SIZE (((size_t)RAMEND + 1) - SRAM_BASE)
+
 typedef enum { unknown, power, external, software, brown_out } reset_type;
 
 reset_type initialize()
@@ -31,7 +44,7 @@ void reset()
 	while (1);
 }
 
-void application()
+void start()
 {
 	asm("jmp 0");
 }
