61134d9788486f549d2388729af50f32c0786abe
Julian Brunner <julian.brunner@gmail.com>
2015-11-09 02:14:01 +0100

    moved defines, added signature and fuse read functionality

--- modules/basic.h
+++ modules/basic.h
@@ -6,22 +6,6 @@
 
 #include <avr/wdt.h>
 
-#define FLASH_BASE ((void*)0)
-#define FLASH_END ((void*)FLASHEND + 1)
-#define FLASH_LENGTH (FLASH_END - FLASH_BASE)
-#define FLASH_PAGE_LENGTH ((size_t)SPM_PAGESIZE)
-#define FLASH_PAGE_COUNT (FLASH_LENGTH / FLASH_PAGE_LENGTH)
-
-#define EEPROM_BASE ((void*)0)
-#define EEPROM_END ((void*)E2END + 1)
-#define EEPROM_LENGTH (EEPROM_END - EEPROM_BASE)
-#define EEPROM_PAGE_LENGTH ((size_t)E2PAGESIZE)
-#define EEPROM_PAGE_COUNT (EEPROM_LENGTH / EEPROM_PAGE_LENGTH)
-
-#define SRAM_BASE ((void*)RAMSTART)
-#define SRAM_END ((void*)RAMEND + 1)
-#define SRAM_LENGTH (SRAM_END - SRAM_BASE)
-
 typedef enum { unknown, power, external, software, brown_out } reset_type;
 
 reset_type initialize()
--- modules/memory.h
+++ modules/memory.h
@@ -7,6 +7,29 @@
 
 #include "basic.h"
 
+#define FLASH_BASE ((void*)0)
+#define FLASH_END ((void*)FLASHEND + 1)
+#define FLASH_LENGTH (FLASH_END - FLASH_BASE)
+#define FLASH_PAGE_LENGTH ((size_t)SPM_PAGESIZE)
+#define FLASH_PAGE_COUNT (FLASH_LENGTH / FLASH_PAGE_LENGTH)
+
+#define EEPROM_BASE ((void*)0)
+#define EEPROM_END ((void*)E2END + 1)
+#define EEPROM_LENGTH (EEPROM_END - EEPROM_BASE)
+#define EEPROM_PAGE_LENGTH ((size_t)E2PAGESIZE)
+#define EEPROM_PAGE_COUNT (EEPROM_LENGTH / EEPROM_PAGE_LENGTH)
+
+#define SRAM_BASE ((void*)RAMSTART)
+#define SRAM_END ((void*)RAMEND + 1)
+#define SRAM_LENGTH (SRAM_END - SRAM_BASE)
+
+#define SIGNATURE_LENGTH 0x10
+
+#define FUSE_LENGTH 0x10
+
+// TODO: definition missing in avr-libc for the atmega328p
+#define SIGRD 5
+
 void flash_read_page(uint8_t page_index, void* data)
 {
 	void* page_position = FLASH_BASE + page_index * FLASH_PAGE_LENGTH;
@@ -34,12 +57,25 @@ void flash_write_page(uint8_t page_index, void* data)
 
 void eeprom_read_page(uint8_t page_index, void* data)
 {
-	// TODO: what happens if this is used beyond the eeprom area? how does this affect verification ack?
 	eeprom_read_block(data, EEPROM_BASE + page_index * EEPROM_PAGE_LENGTH, EEPROM_PAGE_LENGTH);
 }
 void eeprom_write_page(uint8_t page_index, void* data)
 {
 	eeprom_update_block(EEPROM_BASE + page_index * EEPROM_PAGE_LENGTH, data, EEPROM_PAGE_LENGTH);
+}
+
+void signature_read(void* data)
+{
+	uint8_t* bytes = data;
+	for (uint8_t byte_index = 0; byte_index < SIGNATURE_LENGTH; byte_index++)
+		*bytes++ = boot_signature_byte_get(byte_index);
+}
+
+void fuse_read(void* data)
+{
+	uint8_t* bytes = data;
+	for (uint8_t byte_index = 0; byte_index < FUSE_LENGTH; byte_index++)
+		*bytes++ = boot_lock_fuse_bits_get(byte_index);
 }
 
 uint8_t decode_byte(void** offset)
