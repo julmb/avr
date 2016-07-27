5e87c050341a5aa975af232c47c215b0b9edeca6
Julian Brunner <julian.brunner@gmail.com>
2016-07-27 18:53:04 +0200

    added interrupt settings, refactoring

--- modules/usart.h
+++ modules/usart.h
@@ -12,23 +12,23 @@
 	#error "The USART module is not available on the chosen MCU."
 #endif
 
-// full receive buffer sets this flag
-// reset or reading UDR0 clear this flag
-inline uint8_t usart_received_byte() { return UCSR0A & _BV(RXC0); }
 // reset clears this flag
 // empty transmit buffer and empty transmit shift register set this flag
 inline uint8_t usart_sent_byte() { return UCSR0A & _BV(TXC0); }
 // full transmit buffer clears this flag
 // reset or empty transmit buffer set this flag
 inline uint8_t usart_needs_byte() { return UCSR0A & _BV(UDRE0); }
+// full receive buffer sets this flag
+// reset or reading UDR0 clear this flag
+inline uint8_t usart_received_byte() { return UCSR0A & _BV(RXC0); }
 
-inline void usart_wait_receive()
-{
-	while (!usart_received_byte());
-}
 inline void usart_wait_send()
 {
 	while (!usart_sent_byte());
+}
+inline void usart_wait_receive()
+{
+	while (!usart_received_byte());
 }
 
 uint8_t usart_try(uint16_t tick_count)
@@ -49,19 +49,6 @@ uint8_t usart_try(uint16_t tick_count)
 	return 1;
 }
 
-uint8_t usart_read(void* data, size_t length)
-{
-	uint8_t* bytes = data;
-
-	for (size_t index = 0; index < length; index++)
-	{
-		if (usart_try(1)) return 1;
-
-		*bytes++ = UDR0;
-	}
-
-	return 0;
-}
 void usart_write(void* data, size_t length)
 {
 	uint8_t* bytes = data;
@@ -75,12 +62,25 @@ void usart_write(void* data, size_t length)
 		UDR0 = *bytes++;
 	}
 }
+uint8_t usart_read(void* data, size_t length)
+{
+	uint8_t* bytes = data;
 
-void usart_initialize(uint8_t receiver, uint8_t transmitter, uint16_t divider, uint8_t double_speed)
+	for (size_t index = 0; index < length; index++)
+	{
+		if (usart_try(1)) return 1;
+
+		*bytes++ = UDR0;
+	}
+
+	return 0;
+}
+
+void usart_initialize(uint8_t transmitter, uint8_t receiver, uint8_t sent_interrupt, uint8_t needs_interrupt, uint8_t received_interrupt, uint16_t divider, uint8_t double_speed)
 {
 	// set double speed mode (U2X0 = double_speed)
 	// disable multi-processor communication mode (MPCM0 = 0b0)
-	// disable all interrupts (RXCIE0 = 0b0, TXCIE0 = 0b0, UDRIE0 = 0b0)
+	// set interrupts (RXCIE0 = received_interrupt, TXCIE0 = sent_interrupt, UDRIE0 = needs_interrupt)
 	// set receiver and transmitter (RXEN0 = receiver, TXEN0 = transmitter)
 	// set mode to asynchronous USART (UMSEL0 = 0b00)
 	// set parity mode to disabled (UPM0 = 0b00)
@@ -88,7 +88,7 @@ void usart_initialize(uint8_t receiver, uint8_t transmitter, uint16_t divider, u
 	// set character size to 8 bit (UCSZ0 = 0b011)
 	// set clock polarity bit to zero, not used in asynchronous mode (UCPOL0 = 0b0)
 	UCSR0A = ((double_speed ? 1 : 0) << U2X0) | (0 << MPCM0);
-	UCSR0B = (0 << RXCIE0) | (0 << TXCIE0) | (0 << UDRIE0) | ((receiver ? 1 : 0) << RXEN0) | ((transmitter ? 1 : 0) << TXEN0) | (0 << UCSZ02);
+	UCSR0B = ((received_interrupt ? 1 : 0) << RXCIE0) | ((sent_interrupt ? 1 : 0) << TXCIE0) | ((needs_interrupt ? 1 : 0) << UDRIE0) | ((receiver ? 1 : 0) << RXEN0) | ((transmitter ? 1 : 0) << TXEN0) | (0 << UCSZ02);
 	UCSR0C = (0 << UMSEL01) | (0 << UMSEL00) | (0 << UPM01) | (0 << UPM00) | (0 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00) | (0 << UCPOL0);
 
 	// set baud rate
