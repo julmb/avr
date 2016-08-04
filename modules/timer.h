7a93408c2975fbb689ee4aa0f9c840a4deaa61f1
Julian Brunner <julian.brunner@gmail.com>
2016-08-04 02:01:50 +0200

    updated timer module

--- modules/timer.h
+++ modules/timer.h
@@ -5,55 +5,188 @@
 	#error "The timer module is not available on the chosen MCU."
 #endif
 
+// TODO: are these still needed?
 #include <stddef.h>
 #include <stdint.h>
 
 #include <avr/io.h>
 
-inline uint8_t timer_has_elapsed()
+typedef enum { divider_1, divider_8, divider_64, divider_256, divider_1024 } divider;
+
+typedef struct
 {
-	// check for the counter overflow flag
-	return TIFR0 & _BV(TOV0);
+	uint8_t index;
 }
-inline void timer_reset()
+counter;
+
+inline uint8_t counter_get(counter counter)
 {
-	// clear the counter overflow flag
-	TIFR0 |= _BV(TOV0);
+	switch (counter.index)
+	{
+		case 0: return TCNT0;
+		default: return 0;
+	}
 }
-inline void timer_restart()
+inline void counter_set(counter counter, uint8_t value)
 {
-	// reset the counter
-	TCNT0 = 0x00;
+	switch (counter.index)
+	{
+		case 0: TCNT0 = value; break;
+	}
 }
-inline uint8_t timer_value()
+inline uint8_t counter_overflown(counter counter)
 {
-	// get the counter value
-	return TCNT0;
+	switch (counter.index)
+	{
+		case 0: return TIFR0 & _BV(TOV0);
+		default: return 0;
+	}
+}
+inline void counter_reset(counter counter)
+{
+	switch (counter.index)
+	{
+		case 0: TIFR0 |= _BV(TOV0); break;
+	}
 }
 
-void timer_initialize()
+inline uint8_t divider_bits(uint8_t index, divider divider)
 {
-	// disable compare output mode (COM0A = 0b00, COM0B = 0b00)
-	// disable waveform generation mode (WGM0 = 0b000)
-	// set timer to increment every 64 clock cycles, causes overflow every 1 ms at 16 MHz (CS0 = 0b011)
-	TCCR0A = (0 << COM0A1) | (0 << COM0A0) | (0 << COM0B1) | (0 << COM0B0) | (0 << WGM01) | (0 << WGM00);
-	TCCR0B = (0 << WGM02) | (0 << CS02) | (1 << CS01) | (1 << CS00);
-
-	// clear the counter overflow flag
-	TIFR0 |= _BV(TOV0);
-	// reset the counter
-	TCNT0 = 0x00;
+	switch (index)
+	{
+		case 0:
+			switch (divider)
+			{
+				case divider_1: return (0 << CS02) | (0 << CS01) | (1 << CS00);
+				case divider_8: return (0 << CS02) | (1 << CS01) | (0 << CS00);
+				case divider_64: return (0 << CS02) | (1 << CS01) | (1 << CS00);
+				case divider_256: return (1 << CS02) | (0 << CS01) | (0 << CS00);
+				case divider_1024: return (1 << CS02) | (0 << CS01) | (1 << CS00);
+				default: return 0;
+			}
+		default: return 0;
+	}
 }
-void timer_dispose()
-{
-	// reset control registers to their initial values
-	TCCR0A = 0x00;
-	TCCR0B = 0x00;
 
-	// clear the counter overflow flag
-	TIFR0 |= _BV(TOV0);
-	// reset the counter
-	TCNT0 = 0x00;
+inline counter counter_initialize(uint8_t index, divider divider)
+{
+	counter counter =
+	{
+		.index = index
+	};
+
+	switch (counter.index)
+	{
+		case 0:
+			// disable compare output (COM0A = 0b00, COM0B = 0b00)
+			// disable waveform generation (WGM0 = 0b000)
+			// set divider accordingly
+			TCCR0A = (0 << COM0A1) | (0 << COM0A0) | (0 << COM0B1) | (0 << COM0B0) | (0 << WGM01) | (0 << WGM00);
+			TCCR0B = (0 << WGM02) | divider_bits(index, divider);
+			break;
+	}
+
+	counter_reset(counter);
+	counter_set(counter, 0x00);
+
+	return counter;
+}
+inline void counter_dispose(counter counter)
+{
+	switch (counter.index)
+	{
+		case 0:
+			// reset control registers to their initial values
+			TCCR0A = 0x00;
+			TCCR0B = 0x00;
+			break;
+	}
+
+	counter_reset(counter);
+	counter_set(counter, 0x00);
+}
+
+typedef enum { comparer_a, comparer_b } comparer;
+
+typedef struct
+{
+	counter counter;
+	comparer comparer;
+}
+timer;
+
+inline uint8_t timer_get(timer timer)
+{
+	switch (timer.counter.index)
+	{
+		case 0:
+			switch (timer.comparer)
+			{
+				case comparer_a: return OCR0A;
+				case comparer_b: return OCR0B;
+				default: return 0;
+			}
+		default: return 0;
+	}
+}
+inline void timer_set(timer timer, uint8_t value)
+{
+	switch (timer.counter.index)
+	{
+		case 0:
+			switch (timer.comparer)
+			{
+				case comparer_a: return OCR0A = value; break;
+				case comparer_b: return OCR0B = value; break;
+			}
+			break;
+	}
+}
+inline uint8_t timer_elapsed(timer timer)
+{
+	switch (timer.counter.index)
+	{
+		case 0:
+			switch (timer.comparer)
+			{
+				case comparer_a: return TIFR0 & _BV(OCF0A);
+				case comparer_b: return TIFR0 & _BV(OCF0B);
+				default: return 0;
+			}
+		default: return 0;
+	}
+}
+inline void timer_reset(timer timer)
+{
+	switch (timer.counter.index)
+	{
+		case 0:
+			switch (timer.comparer)
+			{
+				case comparer_a: return TIFR0 |= _BV(OCF0A); break;
+				case comparer_b: return TIFR0 |= _BV(OCF0B); break;
+			}
+			break;
+	}
+}
+
+inline timer timer_initialize(counter counter, comparer comparer)
+{
+	timer timer =
+	{
+		.counter = counter,
+		.comparer = comparer
+	};
+
+	timer_reset(timer);
+	timer_set(timer, 0x00);
+
+	return timer;
+}
+inline void timer_dispose(timer timer)
+{
+	timer_reset(timer);
+	timer_set(timer, 0x00);
 }
 
 #endif
